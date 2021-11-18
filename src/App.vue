9b1e7b3e7f7819eed37a04f9518788bfd67c111e
Kujta <kujta27@gmail.com>
2021-11-18 14:42:41 +0100

    worked on the dashboard and added the navlinks to it

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <side-bar :logo="logo"></side-bar>
+  </div>
+</template>
+
+<script>
+import SideBar from './components/layout/SideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar
+  },
+  data() {
+    return {
+      logo: '../assets/bna-invoices.png'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/layout/SideBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="d-flex flex-column flex-shrink-0 p-3 text-muted bg-dark vh-100" style="width: 240px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+    </a>
+    <a :href="logo"></a>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link active" aria-current="page">
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-muted">
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-muted">
+          Orders
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-muted">
+          Products
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-muted">
+          Customers
+        </a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['logo']
+}
+</script>
